7850886dd5273eca3656c4c5c41b9e0feb9e8c59
Dmitry <d.shashkov1998@gmail.com>
2020-02-25 10:24:08 +0300

    first load

---
+++ map.js
@@ -0,0 +1,62 @@
+if (navigator.geolocation) {
+  var Options = {
+    enableHighAccuracy: true,
+    timeout: 1000,
+    maximumAge: 0
+  };
+
+  function success(pos) {
+    init(pos.coords.latitude, pos.coords.longitude);
+    let maps = document.querySelectorAll("#map .ymaps-2-1-75-map");
+    maps[0].remove();
+  }
+
+  function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+  }
+
+  navigator.geolocation.getCurrentPosition(success, error, Options);
+}
+ymaps.ready(init);
+function init(latitude, longitude) {
+  var placeMark1, carPlaceMark, coordsBegin, coordsEnd; //маркеры
+  var myMap = new ymaps.Map("map", {
+    center: [latitude, longitude],
+    zoom: 16
+  });
+  myMap.events.add("click", function(e) {
+    if (coordsBegin === undefined) {
+      placeMark1 = new ymaps.Placemark(e.get("coords"), {
+        balloonContent: "Начало"
+      });
+      myMap.geoObjects.add(placeMark1);
+      coordsBegin = e.get("coords");
+    } else {
+      if (coordsEnd === undefined) {
+        coordsEnd = e.get("coords");
+      }
+      ymaps
+        .route(
+          [
+            { type: "wayPoint", point: coordsBegin },
+            { type: "wayPoint", point: coordsEnd }
+          ],
+          {
+            multiRoute: true
+          }
+        )
+        .done(
+          function(route) {
+            route.options.set("mapStateAutoApply", true);
+            myMap.geoObjects.add(route);
+            var coords = route.getWayPoints();
+            console.log(route);
+          },
+          function(err) {
+            throw err;
+          },
+          this
+        );
+    }
+  });
+}
