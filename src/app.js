83fb14f5b93d9e120e57b6d29b69465414ac61c6
dmitrynjg <49000073+dmitrynjg@users.noreply.github.com>
2020-03-22 16:13:10 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -64,7 +64,8 @@ function initApp() {
       super();
     }
     render() {
-      if (this.getAttribute('preview') === 'true') {
+      let boolPreview = this.getAttribute('preview') === 'true'
+      if (boolPreview) {
         this.innerHTML =
           `<div class ='content-info-item-btns'> <button class='content-info-item-btn content-info-item-btn-order'>Закзать</button>` +
           `<button class='content-info-item-btn content-info-item-btn-cancel'>Отмена</button>` +
